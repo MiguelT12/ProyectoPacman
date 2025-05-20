aefe40c012f40449591c515d0c4e2478fc08658a
MiguelT12 <mtejerogarcia@educa.madrid.org>
2025-05-20 19:49:02 +0200

    Añado la documentación

--- untitled/src/temporal/GestorNiveles.java
+++ untitled/src/temporal/GestorNiveles.java
@@ -1,5 +1,8 @@
-package juego;
+package temporal;
 
+import juego.Nivel;
+import juego.PacmanComidoException;
+import juego.SalirDelJuegoException;
 import multimedia.*;
 
 public class GestorNiveles {
@@ -7,6 +10,7 @@ public class GestorNiveles {
     private Teclado teclado;
     private int nivelActual = 1;
     private Nivel nivel;
+    private int puntuacionPorNivel = 110;
 
     public GestorNiveles(Lienzo lienzo, Teclado teclado) {
         this.lienzo = lienzo;
@@ -14,14 +18,14 @@ public class GestorNiveles {
         cargarNivel(nivelActual);
     }
 
-    private void cargarNivel(int numero) {
+    public void cargarNivel(int numero) {
         nivel = new Nivel(lienzo, teclado, numero);
     }
 
     public void tick() throws SalirDelJuegoException, PacmanComidoException {
         nivel.tick();
 
-        if (nivel.mapaCompletado()){
+        if (nivel.getPuntuacion() >= nivelActual * puntuacionPorNivel){
             nivelActual++;
             cargarNivel(nivelActual);
         }
